8a597f86782d8f89bb679725a3373fc9778186f0
e06-tech <71258352+e06-tech@users.noreply.github.com>
2021-12-31 23:59:30 +0900

    main.go in #4.5

---
+++ #4. Blockchain/main(5).go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"blockchain"
+	"fmt"
+)
+
+func main() {
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
+}

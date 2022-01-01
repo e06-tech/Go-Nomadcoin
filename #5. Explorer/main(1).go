6141b7ae0447caaab6e926bc65008940595898cc
e06-tech <71258352+e06-tech@users.noreply.github.com>
2022-01-01 12:54:04 +0900

    blockchain(4강 blockchain파일에서 일부 수정) / #5.1 main.go

---
+++ #5. Explorer/main(1).go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"blockchain"
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+const port string = ":4000"
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
